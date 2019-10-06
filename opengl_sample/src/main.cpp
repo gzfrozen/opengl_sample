26eff6604cd112bba9f04934be0e087de3e5c354
gzfrozen <gzliuxiaochen@gmail.com>
2019-10-07 00:54:39 +0900

    a prittier trangle

--- opengl_sample/src/main.cpp
+++ opengl_sample/src/main.cpp
@@ -54,9 +54,14 @@ int main()
 		//
 		// ここで描画処理を行う
 		glBegin(GL_TRIANGLES);
-		glVertex2f(-0.5f, -0.5f);
-		glVertex2f(0.0f, 0.5f);
-		glVertex2f(0.5f, -0.5f);
+		{
+			glColor3f(1.0f, 0.0f, 0.0f);
+			glVertex2f(0.0f, +1.0f);
+			glColor3f(0.0f, 1.0f, 0.0f);
+			glVertex2f(-1.0f, -1.0f);
+			glColor3f(0.0f, 0.0f, 1.0f);
+			glVertex2f(+1.0f, -1.0f);
+		}
 		glEnd();
 		//
 		// カラーバッファを入れ替える
